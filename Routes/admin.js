27bf12122f19a7523507ff832b992ae2bb7c6a21
anovozhilova25 <alina.novozhilova12345@gmail.com>
2023-12-13 03:37:21 +0300

    Routes Done

---
+++ Routes/admin.js
@@ -0,0 +1,24 @@
+var express = require('express');
+const adminController = require("../controllers/adminController.js");
+var router = express.Router();
+var bodyParser = require('body-parser');
+var urlencodeParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = express.json();
+
+router.ger('/',adminController.getArticleAll);
+router.ger('/:idArticle',adminController.getArticle);
+router.ger('/:addArticle',adminController.addArticle);
+router.ger('/:editArticle',adminController.editArticle);
+router.ger('/:deleteArticle',adminController.deleteArticle);
+
+module.exports = router;
+
+
+/*adminController.getArticleAll – Получить список всех статей
+adminController.getArticle - Получить текст статьи
+adminController.addArticle – Добавить статью
+adminController.editArticle – Редактировать статью
+adminController.deleteArticle – Удалить статью
+articleController.getArticleAll – Получить список всех статей
+articleController.getArticle – Получить текст статьи
+homeController.getIndex – получить ссылку на главную*/
