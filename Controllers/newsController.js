2f3a23de59b72511721c43961901f4b0d370ecbe
anovozhilova25 <alina.novozhilova12345@gmail.com>
2023-12-15 03:56:46 +0300

    J

--- Controllers/newsController.js
+++ Controllers/newsController.js
@@ -2,11 +2,11 @@ const NewsModel = require('../Models/newsModel');
 
 exports.getNewsAll = async (req, res) => {
     const newsList = await NewsModel.getAllNews();
-    res.render('newsList', {newsList});
+    res.render('newsList', { newsList });
 };
 
 exports.getNews = async (req, res) => {
     const newsId = req.params.idNews;
     const news = await NewsModel.getNewsById(newsId);
-    res.render('newsContent', {news});
+    res.render('newsDetail', { news });
 };
